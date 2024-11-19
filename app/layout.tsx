4d50159cbc41d76b80d79722d3a04c881c246b59
Hasir Ali <hasira804@gmail.com>
2024-11-19 15:02:48 +0530

    config

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,8 +56,8 @@ const workSans = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "YC Directory",
-  description: "Pitch, Vote and Grow",
+  title: "The Daily Journal",
+  description: "A journaling app for everyone",
 };
 
 export default function RootLayout({
